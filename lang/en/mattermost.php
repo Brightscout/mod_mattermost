09536ba0c5710520452811b187ba636bd18e8c08
Manoj <manoj@brightscout.com>
2021-07-26 14:19:57 +0530

    Initial setup and created the settings page

---
+++ lang/en/mattermost.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_mattermost
+ * @category    string
+ * @copyright   2020 Manoj <manoj@brightscout.com>
+ * @author Manoj <manoj@brightscout.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Mattermost';
+
+$string['modulename_link'] = 'mod/mattermost';
+$string['name'] = 'Instance name (in the course)';
+
+$string['settings'] = 'Mattermost plugin settings';
+$string['testconnection'] = 'Test connection to Mattermost';
+
+$string['instanceurl'] = 'Mattermost instance url';
+$string['instanceurl_desc'] = 'Mattermost instance URL (ex: https://mm.moodle.com)';
+$string['secret'] = 'Mattermost webhook secret';
+$string['secret_desc'] = 'Mattermost associated webhook secret';
+$string['oauthservice'] = 'Mattermost Oauth service';
+$string['oauthservice_desc'] = 'Oauth service used in Mattermost (LDAP or SAML)';
+$string['groupnametoformat'] = 'Formatted group name';
+$string['groupnametoformat_desc'] = 'String format {$a->parameter} is possible with the following parameters : moodleid, moodleshortname, moodlefullname, moduleid, modulemoodleid (unique whitin all your possible moodle), courseid, courseshortname, coursefullname';
+$string['create_user_account_if_not_exists'] = 'Create Mattermost user account';
+$string['create_user_account_if_not_exists_desc'] = 'While enrolling user, create Mattermost corresponding user account(username), if it does not exist';
+$string['validationgroupnameregex'] = 'Mattermost group validation name regular expression to remove invalid characters';
+$string['validationgroupnameregex_desc'] = 'Moodle will replace every unauthorized caracters by _. This regexp is the exact negation of the Mattermost server one concerning group name validation';
+$string['background_add_instance'] = 'Pass Mattermost enrolments to background task while creating a new module instance';
+$string['background_add_instance_desc'] = 'This will prevent waiting of the module creation page';
+$string['background_restore'] = 'Pass Mattermost enrolments to background task while dupplicating a mattermost modules';
+$string['background_restore_desc'] = 'This will prevent waiting while dupplicating a Mattermost module';
+$string['background_user_update'] = 'Pass Mattermost enrolments to background task while updating user informations such as activation/deactivation.';
+$string['background_user_update_desc'] = 'Pass Mattermost enrolments to background task while updating user informations such as activation/deactivation.';
+
+
---
+++ settings.php
@@ -0,0 +1,168 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin administration pages are defined here.
+ *
+ * @package     mod_mattermost
+ * @category    admin
+ * @copyright   2020 Manoj <manoj@brightscout.com>
+ * @author Manoj <manoj@brightscout.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// require_once($CFG->dirroot.'/mod/mattermost/locallib.php');
+require_once($CFG->libdir . '/enrollib.php');
+// Make sure core is loaded.
+
+// Redefine the H5P admin menu entry to be expandable.
+$modmattermostfolder = new admin_category(
+    'modmattermostfolder',
+    new lang_string('pluginname', 'mod_mattermost'),
+    $module->is_enabled() === false
+);
+// Add the Settings admin menu entry.
+$ADMIN->add('modsettings', $modmattermostfolder);
+$settings->visiblename = new lang_string('settings', 'mod_mattermost');
+// Add the Libraries admin menu entry.
+$ADMIN->add('modmattermostfolder', $settings);
+$ADMIN->add('modmattermostfolder', new admin_externalpage(
+    'mattermostconnectiontest',
+    new lang_string('testconnection', 'mod_mattermost'),
+    new moodle_url('/mod/mattermost/test_connection.php')
+));
+
+if ($ADMIN->fulltree) {
+    $settings->add(
+        new admin_setting_configtext(
+            'mod_mattermost/instanceurl',
+            get_string('instanceurl', 'mod_mattermost'),
+            get_string('instanceurl_desc', 'mod_mattermost'),
+            null,
+            PARAM_URL
+        )
+    );
+    $settings->add(
+        new admin_setting_configpasswordunmask(
+            'mod_mattermost/secret',
+            get_string('secret', 'mod_mattermost'),
+            get_string('secret_desc', 'mod_mattermost'),
+            ''
+        )
+    );
+    $settings->add(
+        new admin_setting_configtext(
+            'mod_mattermost/oauthservice',
+            get_string('oauthservice', 'mod_mattermost'),
+            get_string('oauthservice_desc', 'mod_mattermost'),
+            null,
+            PARAM_TEXT
+        )
+    );
+    $settings->add(
+        new admin_setting_configtext('mod_mattermost/groupnametoformat',
+            get_string('groupnametoformat', 'mod_mattermost'),
+            get_string('groupnametoformat_desc', 'mod_mattermost'),
+            '{$a->moodleid}_{$a->courseshortname}_{$a->moduleid}'
+        )
+    );
+
+
+    // $rolesoptions = role_fix_names(get_all_roles(), null, ROLENAME_ORIGINALANDSHORT, true);
+    // $editingteachers = get_archetype_roles('editingteacher');
+    // $student = get_archetype_roles('student');
+    // $settings->add(
+    //     new admin_setting_configmultiselect('mod_mattermost/defaultmoderatorroles',
+    //         get_string('defaultmoderatorroles', 'mod_mattermost'),
+    //         get_string('defaultmoderatorroles_desc', 'mod_mattermost'),
+    //         array_keys($editingteachers),
+    //         $rolesoptions
+    //     )
+    // );
+
+    // $settings->add(
+    //     new admin_setting_configmultiselect('mod_mattermost/defaultuserroles',
+    //         get_string('defaultuserroles', 'mod_mattermost'),
+    //         get_string('defaultuserroles_desc', 'mod_mattermost'),
+    //         array_keys($student),
+    //         $rolesoptions
+    //     )
+    // );
+
+    $settings->add(
+        new admin_setting_configcheckbox('mod_mattermost/create_user_account_if_not_exists',
+            get_string('create_user_account_if_not_exists', 'mod_mattermost'),
+            get_string('create_user_account_if_not_exists_desc', 'mod_mattermost'),
+            1
+        )
+    );
+
+    $settings->add(
+        new admin_setting_configtext('mod_mattermost/validationgroupnameregex',
+            get_string('validationgroupnameregex', 'mod_mattermost'),
+            get_string('validationgroupnameregex_desc', 'mod_mattermost'),
+                '/[^0-9a-zA-Z-_.]/'
+        )
+    );
+
+    // $enabledenrolmentplugins = enrol_get_plugins(true);
+    // $enabledenrolmentplugins = array_keys($enabledenrolmentplugins);
+    // array_walk($enabledenrolmentplugins,
+    //     function(&$value, $key){
+    //         $value = 'enrol_'.$value;
+    //     }
+    // );
+    // $enabledenrolmentplugins = array_combine($enabledenrolmentplugins, $enabledenrolmentplugins);
+    // $default = array(
+    //     'enrol_flatfile' => 'enrol_flatfile',
+    //     'enrol_cohort' => 'enrol_cohort'
+    // );
+    // $settings->add(
+    //     new admin_setting_configmultiselect('mod_mattermost/background_enrolment_task',
+    //         get_string('background_enrolment_task', 'mod_mattermost'),
+    //         get_string('background_enrolment_task_desc', 'mod_mattermost'),
+    //         $default,
+    //         $enabledenrolmentplugins
+    //     )
+    // );
+    $settings->add(new admin_setting_configcheckbox('mod_mattermost/background_add_instance',
+        get_string('background_add_instance', 'mod_mattermost'),
+        get_string('background_add_instance_desc', 'mod_mattermost'),
+        1
+    ));
+
+    $settings->add(new admin_setting_configcheckbox(
+        'mod_mattermost/background_restore', get_string('background_restore', 'mod_mattermost'),
+        get_string('background_restore_desc', 'mod_mattermost'),
+        1
+    ));
+
+    // $settings->add(new admin_setting_configcheckbox(
+    //     'mod_mattermost/background_synchronize', get_string('background_synchronize', 'mod_mattermost'),
+    //     get_string('background_synchronize_desc', 'mod_mattermost'),
+    //     1
+    // ));
+
+    $settings->add(new admin_setting_configcheckbox(
+        'mod_mattermost/background_user_update', get_string('background_user_update', 'mod_mattermost'),
+        get_string('background_user_update_desc', 'mod_mattermost'),
+        1
+    ));
+}
+// Prevent Moodle from adding settings block in standard location.
+$settings = null;
